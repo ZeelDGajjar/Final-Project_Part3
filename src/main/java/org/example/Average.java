af88d953edb04202ebf17385f517a5bf42f11899
ZeelDGajjar <2474008@edu.vaniercollege.qc.ca>
2024-11-24 18:31:20 -0500

    Completed Average.java

--- src/main/java/org/example/Average.java
+++ src/main/java/org/example/Average.java
@@ -1,9 +1,68 @@
 package org.example;
-
-
+import java.util.Scanner;
 
 public class Average {
-    public static void main(String[] args) {
-        System.out.println("Average is ");
+
+    //Data fields
+    private int[] data;
+    private double mean;
+
+    Scanner info = new Scanner(System.in);
+
+    public Average(){
+        for(int i = 0; i < 5; i++){
+            System.out.printf("Please enter score %d", i + 1);
+            data[i] = info.nextInt();
+
+            calculateMean();
+            selectionSort();
+        }
     }
+
+    public void calculateMean(){
+        double total = 0;
+        for(int i = 0; i < 5; i++){
+            total += data[i];
+        }
+        mean = total / data.length;
+    }
+
+    public String toString(){
+        int[] data = new int[4]; ///Can we put five exactly?
+
+        for(int i = 0; i < 5; i++){
+            if(data[i] < data[i+1]){
+                int num1 = data[i];
+                data[i] = data[i + 1];
+                data[i + 1] = num1;
+            }
+        }
+
+        int total = 0;
+        for(int i = 0; i < 5; i++){
+            total += data[i];
+        }
+
+        //double mean = total/5;
+
+        return data.toString() + Integer.valueOf(total/5); ///not sure. does this include all? //how do I add mean in it??
+    }
+
+    public void selectionSort(){ //Still not sure
+        int arLength = data.length;
+        for (int i = 0; i < arLength; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arLength; j++) {
+                if (data[j] > data[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = data[minIndex];
+            data[minIndex] = data[i];
+            data[i] = temp;
+        }
+
+    }
+
 }
