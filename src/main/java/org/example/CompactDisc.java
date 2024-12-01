33322296af63d73a627e35eb6bf35d8880e14b91
ZeelDGajjar <2474008@edu.vaniercollege.qc.ca>
2024-12-01 17:52:23 -0500

    created AverageDriver.java; trying to debug Average. java; Started working in Average.drive; need to debug

--- src/main/java/org/example/CompactDisc.java
+++ src/main/java/org/example/CompactDisc.java
@@ -1,4 +1,6 @@
 package org.example;
+
+
 import java.util.Scanner;
 import java.io.*;
 /**
@@ -7,31 +9,38 @@ import java.io.*;
  */
 public class CompactDisc
 {
-    public static void main(String[] args)throws IOException
-    {
-        Scanner input = new Scanner(new File("Classics.txt"));
+    public static void main(String[] args)throws IOException{
+
+        Scanner input = new Scanner(new File("src/main/resources/Classics.txt"));
         String title;
         String artist;
 
         // ADD LINES FOR TASK #3 HERE
         // Declare an array of Song objects, called cd, // with
-        private int[] cd = int[6];
 
-        a size of 6
-        for (int i = 0; i < cd.length; i++)
-        {
+        Song[] cd = new Song[6];
+
+        for (int i = 0; i < cd.length; i++) {
             title = input.nextLine();
             artist = input.nextLine();
-// ADD LINES FOR TASK #3 HERE
-// Fill the array by creating a new song with
-// the title and artist and storing it in the
-// appropriate position in the array
+
+            // ADD LINES FOR TASK #3 HERE
+            cd[i] = new Song(title, artist); // Fill the array by creating a new song with
+            // the title and artist and storing it in the
+            // appropriate position in the array
         }
+
+        input.close();
+
         System.out.println("Contents of Classics:");
-        for (int i = 0; i < cd.length; i++)
-        {
-// ADD LINES FOR TASK #3 HERE
-// Print the contents of the array to the console
+
+        for (int i = 0; i < cd.length; i++) {
+            // ADD LINES FOR TASK #3 HERE
+            // Print the contents of the array to the console
+            System.out.println(cd[i].toString());
         }
     }
+
+
+
 }
