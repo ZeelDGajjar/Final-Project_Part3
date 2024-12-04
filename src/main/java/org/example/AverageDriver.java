02873a0e7fd111d0d328f5e0f346ae202f47d2a8
Z.G. <2474008@edu.vaniercollege.qc.ca>
2024-12-04 14:59:38 -0500

    Update AverageDriver.java

--- src/main/java/org/example/AverageDriver.java
+++ src/main/java/org/example/AverageDriver.java
@@ -3,8 +3,6 @@ import java.util.*;
 
 public class AverageDriver {
     public static void main(String[] args) {
-         System.out.println("Enter the number of scores: ");
-         Scanner info = new Scanner(System.in); ///Do we need to do this??
 
          Average average = new Average();
 
