e4cd4d079cf07d5f14874672bf0a836273edf000
Z.G. <2474008@edu.vaniercollege.qc.ca>
2024-12-08 21:58:48 -0500

    Update AverageDriver.java

--- src/main/java/org/example/AverageDriver.java
+++ src/main/java/org/example/AverageDriver.java
@@ -1,16 +1,12 @@
 package org.example;
-import java.util.*;
 
 /**
- *
  * @author 2474008
- */
+ * */
 
 public class AverageDriver {
     public static void main(String[] args) {
-
-         Average average = new Average();
-
+        Average average = new Average();
         System.out.println(average);
     }
 }
